734275d0ec2cade44659e8b326bd8f1208e66918
Achim Friedland <github@ahzf.de>
2012-02-21 15:50:46 +0100

    Added graphdb semantics.

---
+++ Eunomia/Semantics.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Copyright (c) 2011-2012, Achim 'ahzf' Friedland <code@ahzf.de>
+ * This file is part of Eunomia <http://www.github.com/ahzf/Eunomia>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace de.ahzf.Eunomia
+{
+
+    /// <summary>
+    /// The graph-database ontology.
+    /// </summary>
+    public static class Semantics
+    {
+
+        public static String GraphDBPrefix  = "http://graph-database.org/graphdb/0.1";
+
+        public static String Id             = "Id";
+        public static String RevId          = "RevId";
+        public static String Description    = "Description";
+
+    }
+
+}
